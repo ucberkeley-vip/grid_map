1461cec10494e8e55a3ac6f88062a8721a09f250
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2022-01-26 12:55:14 -0800

    Added submap image

--- grid_map_pcl/src/grid_map_pcl_from_pointcloud2.cpp
+++ grid_map_pcl/src/grid_map_pcl_from_pointcloud2.cpp
@@ -9,7 +9,7 @@
 #include <ros/package.h>
 
 #include <grid_map_core/GridMap.hpp>
-#include <grid_map_ros/GridMapRosConverter.hpp>
+#include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_cv/grid_map_cv.hpp>
 
 #include <pcl_conversions/pcl_conversions.h>
@@ -28,6 +28,7 @@
 #include "opencv2/highgui.hpp"
 #include <opencv2/core/eigen.hpp>
 
+#include <tf/transform_listener.h>
 
 #include "sensor_msgs/PointCloud2.h"
 
@@ -39,7 +40,8 @@ ros::Publisher gridImagePub;
 ros::Subscriber sub;
 
 grid_map::GridMapPclLoader gridMapPclLoader;
-//ros::NodeHandle nh("~");
+
+tf::TransformListener* tf_listener;
 
 void pointcloud_callback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
     // maybe move all process into this subscriber callback function
@@ -47,6 +49,12 @@ void pointcloud_callback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
     ros::NodeHandle nh("~");
 
     ROS_INFO("Pointcloud dim: %d x %d", cloud_msg->height, cloud_msg->width);
+
+    // sensor_msgs::PointCloud2 transformed;
+    // bool success = pcl_ros::transformPointCloud("base_link", *cloud_msg, transformed, *tf_listener);
+    // if (!success) {
+    //     ROS_WARN("transformPointCloud to base_link failed");
+    // }
 
     pcl::PCLPointCloud2 pcl_pc2;
     pcl_conversions::toPCL(*cloud_msg, pcl_pc2);
@@ -63,6 +71,7 @@ void pointcloud_callback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
     grid_map::GridMap gridMap;
     grid_map::GridMapCvProcessing::changeResolution(originalMap, gridMap, 0.001);
     gridMap.setFrameId(gm::getMapFrame(nh));
+    // gridMap.setFrameId(*cloud_msg.header.frame_id);
 
     // std::vector<std::string> layer_name = gridMap.getLayers();
     // for (auto &name : layer_name) {
@@ -75,19 +84,35 @@ void pointcloud_callback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
     // ROS_INFO("grid map column size: %s", grid_map_col.c_str());
     // ROS_INFO("grid map row size: %s", grid_map_row.c_str());
 
-    grid_map::Matrix m = gridMap["elevation"];
-    cv::Mat raw;
-    cv::eigen2cv(m, raw);
+    bool success;
+    grid_map::Position pos = gridMap.getPosition();
+    ROS_INFO("Grid map position: %f %f\n", pos(0), pos(1));
+    ROS_INFO("Grid map length: %f %f\n", gridMap.getLength()(0), gridMap.getLength()(1));
+    grid_map::Length length(2.0, 2.0);
+    grid_map::GridMap localMap = gridMap.getSubmap(pos, length, success);
+    if (!success) {
+        ROS_WARN("getting local submap failed");
+    }
+    // localMap.setGeometry(gridMap.getLength());
+
+    // grid_map::Index submapStartIndex(3, 5);
+    // grid_map::Index submapBufferSize(12, 7);
+
+    // for (grid_map::SubmapIterator iterator(gridMap, submapStartIndex, submapBufferSize);
+    //     !iterator.isPastEnd(); ++iterator) {
+    //     localMap.at("elevation", *iterator) = gridMap.at("elevation", *iterator);
+    // }
+
+
     cv::Mat map;
-    grid_map::GridMapCvConverter::toImage<unsigned char, 1>(gridMap, "elevation", CV_8UC1, map);
+    grid_map::GridMapCvConverter::toImage<unsigned char, 1>(localMap, "elevation", CV_8UC1, map);
     auto pathName = ros::package::getPath("grid_map_pcl");
     ROS_INFO("package name: %s", pathName.c_str());
-    cv::imwrite(pathName + "/raw_image.png", raw);
-    cv::imwrite(pathName + "/depth_image.png", map);
+    cv::imwrite(pathName + "/local_image.png", map);
     
-    double min, max;
-    cv::minMaxLoc(raw, &min, &max);
-    ROS_INFO("image data: %f %f", min, max);
+    // double min, max;
+    // cv::minMaxLoc(raw, &min, &max);
+    // ROS_INFO("image data: %f %f", min, max);
 
     // cv::imshow("Grid map", map);
     // cv::waitKey(1);
@@ -122,6 +147,8 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "grid_map_from_pointcloud2");
     ros::NodeHandle nh("~");
     gm::setVerbosityLevelToDebugIfFlagSet(nh);
+
+    tf_listener = new tf::TransformListener();
 
     gridMapPclLoader.loadParameters(gm::getParameterPath(nh));
 
