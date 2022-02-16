408a2c1624e03def6ed751776a8d35f7f721c042
Yibin_Li <yam61ht@outlook.com>
2022-02-15 20:33:39 -0800

    added submap

--- grid_map_pcl/src/grid_map_pcl_from_pointcloud2_with_odom.cpp
+++ grid_map_pcl/src/grid_map_pcl_from_pointcloud2_with_odom.cpp
@@ -19,6 +19,7 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
+#include <pcl/filters/passthrough.h>
 
 #include <opencv2/core/eigen.hpp>
 #include <Eigen/Dense>
@@ -44,6 +45,7 @@
 namespace gm = ::grid_map::grid_map_pcl;
 
 ros::Publisher gridMapPub;
+ros::Publisher localPointCloudPub;
 ros::Subscriber sub;
 
 grid_map::GridMapPclLoader gridMapPclLoader;
@@ -52,7 +54,7 @@ grid_map::GridMapPclLoader gridMapPclLoader;
 //void pointcloud_callback (sensor_msgs::PointCloud2ConstPtr& cloud_msg, nav_msgs::Odometry::ConstPtr& odom_msg){
 void pointcloud_callback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const nav_msgs::Odometry::ConstPtr& odom_msg){
     // maybe move all process into this subscriber callback function
-    bool save_grid_map_to_local = false;
+    bool save_grid_map_to_local = true;
     ROS_INFO("inside callback");
     ros::NodeHandle nh("~");
 
@@ -60,14 +62,48 @@ void pointcloud_callback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg, con
     pcl_conversions::toPCL(*cloud_msg,pcl_pc2);
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud_input(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromPCLPointCloud2(pcl_pc2,*pcl_cloud_input);
+
+    double odom_pose_position_x = odom_msg->pose.pose.position.x;
+    double odom_pose_position_y = odom_msg->pose.pose.position.y;
+    double odom_pose_position_z = odom_msg->pose.pose.position.z;
+
     ROS_INFO("pointcloud2 to pcl pointcloud conversion succeed!");
     ROS_INFO("frame id : %s", odom_msg->header.frame_id.c_str());
+    ROS_INFO("odom time : %f", odom_msg->header.stamp.toSec());
+    ROS_INFO("map time : %f", cloud_msg->header.stamp.toSec());
     ROS_INFO("position x: %f", odom_msg->pose.pose.position.x);
     ROS_INFO("position y: %f", odom_msg->pose.pose.position.y);
     ROS_INFO("position z: %f", odom_msg->pose.pose.position.z);
 
+    // process point cloud and get local point cloud here. Should probably do it elsewhere
+    pcl::PointCloud<pcl::PointXYZ> xf_cloud, yf_cloud, zf_cloud;
+    pcl::PassThrough<pcl::PointXYZ> pass_x;
+    pass_x.setInputCloud(pcl_cloud_input);
+    pass_x.setFilterFieldName("x");
+    pass_x.setFilterLimits(-1.0 + odom_pose_position_x,1.0 + odom_pose_position_x);
+    pass_x.filter(xf_cloud);
 
-    gridMapPclLoader.setInputCloud(pcl_cloud_input);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr xf_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>(xf_cloud));
+    pcl::PassThrough<pcl::PointXYZ> pass_y;
+    pass_y.setInputCloud(xf_cloud_ptr);
+    pass_y.setFilterFieldName("y");
+    pass_y.setFilterLimits(-1.0 + odom_pose_position_x, 1.0 + odom_pose_position_y);
+    pass_y.filter(yf_cloud);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr yf_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>(yf_cloud));
+    pcl::PassThrough<pcl::PointXYZ> pass_z;
+    pass_z.setInputCloud(yf_cloud_ptr);
+    pass_z.setFilterFieldName("z");
+    pass_z.setFilterLimits(-3.0 + odom_pose_position_z, 3.0 + odom_pose_position_z);
+    pass_z.filter(zf_cloud);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr zf_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>(zf_cloud));
+    sensor_msgs::PointCloud2 LocalPointsMsg;
+    pcl::toROSMsg(*zf_cloud_ptr, LocalPointsMsg);
+    localPointCloudPub.publish(LocalPointsMsg);
+
+//    gridMapPclLoader.setInputCloud(pcl_cloud_input); // global point cloud
+    gridMapPclLoader.setInputCloud(zf_cloud_ptr); // local point cloud
     gridMapPclLoader.loadParameters(ros::package::getPath("grid_map_pcl") + "/config/parameters.yaml");
     gm::processPointcloud(&gridMapPclLoader, nh);
 
@@ -96,23 +132,36 @@ void pointcloud_callback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg, con
     ROS_INFO("=========================================================");
     ROS_INFO(" ");
 
+//    bool success;
+//    grid_map::Position pos = gridMap.getPosition();
+//    grid_map::Position pos = grid_map::Position(odom_msg->pose.pose.position.x, odom_msg->pose.pose.position.y); // use real odom info
+//    ROS_INFO("Grid map position: %f %f\n", pos(0), pos(1));
+//    ROS_INFO("Grid map length: %f %f\n", gridMap.getLength()(0), gridMap.getLength()(1));
+//    grid_map::Length length(2.0, 2.0);
+//    grid_map::GridMap localMap = gridMap.getSubmap(pos, length, success);
+//    if (!success) {
+//        ROS_WARN("getting local submap failed");
+//    }
+
     if (save_grid_map_to_local) {
         cv::Mat map;
         grid_map::GridMapCvConverter::toImage<unsigned char,3>(gridMap, "elevation", CV_16UC3, map);
+//        grid_map::GridMapCvConverter::toImage<unsigned char,3>(localMap, "elevation", CV_16UC3, map);
+//        cv::imwrite("/home/viplab/grid_map_output/grid_map_img_from_cv_converter_" + odom_msg->header.stamp + ".png", map);
         cv::imwrite("/home/viplab/grid_map_output/grid_map_img_from_cv_converter.png", map);
 
-//        eigen matrix to opencv image
-        cv::Mat map_from_eigen;
-
-        eigen2cv(m, map_from_eigen);
-        cv::imwrite("/home/viplab/map_from_eigen.png", map_from_eigen);
-
-//        eigen matrix to local txt file
-        std::ofstream file("/home/viplab/grid_map_output/grid_map_eigen.txt");
-        if (file.is_open())
-        {
-            file << m << '\n';
-        }
+////        eigen matrix to opencv image
+//        cv::Mat map_from_eigen;
+//
+//        eigen2cv(m, map_from_eigen);
+//        cv::imwrite("/home/viplab/map_from_eigen.png", map_from_eigen);
+//
+////        eigen matrix to local txt file
+//        std::ofstream file("/home/viplab/grid_map_output/grid_map_eigen.txt");
+//        if (file.is_open())
+//        {
+//            file << m << '\n';
+//        }
     }
 
     // publish grid map
@@ -141,6 +190,7 @@ int main(int argc, char** argv) {
 //    sub = nh.subscribe(gm::getPointcloudTopic(nh), 1, pointcloud_callback);
 
     gridMapPub = nh.advertise<grid_map_msgs::GridMap>("grid_map_from_raw_pointcloud", 1, true);
+    localPointCloudPub = nh.advertise<sensor_msgs::PointCloud2>("/local_map", 100);
 
 /*
 //    const std::string pathToCloud = gm::getPcdFilePath(nh);
